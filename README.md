5e185a50b6319084e46fec32b6f4503cb114214e
Toby Hall <toby.m.hall@gmail.com>
2018-01-07 19:47:52 -0500

    added readme and sample event data. minor functionality updates.

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # longpoint_league_importer
+
+Simple CSV reader & writer for importing tournament results for Historical European Martial Arts and calculating league standings
+
+## Usage
+
+Prepare csv files for import using two columns with the headers `name_key` and `place`. `name_key` values should be consistent for a fighter across multiple tournaments, and the `place` value should be an integer indicating that fighter's final ranking for that tournament.
+
+For a simple import and export, from the command line run:
+
+```
+$ python league_rating.py -f filename [additional_filenames] -w
+filename import successful
+1 event file(s) imported
+Output written to file default.csv
+```
+Additional flags:
+
+`-e` Requests user input for overriding input filenames in export headers
+```
+$ python league_rating.py -f filename [additional_filename] -we
+Enter event name for filename: foobar
+filename import successful
+1 event file(s) imported
+Enter max # of events for use in scoring: 2
+Output written to default.csv
+```
+
+`-o` Input to override default export name
+```
+$ python league_rating.py -f filename [additional_filenames] -wo export_filename
+filename import successful
+1 event file(s) imported
+Output written to file export_filename
+```
+
+## Example Usage
+
+```
+$ python league_rating.py -f tests/test_1.csv tests/test_2.csv tests/test_3.csv -weo test_export.csv
+```
--- league_rating.py
+++ league_rating.py
@@ -12,8 +12,15 @@ POINTS_MAP = [
 	{"max_place":32, "points":1},
 ]
 
-class Importer(object):
+# constant strings for output
+LEAGUE_POINTS = 'league_points'
+TIE_BREAK = 'tie_break'
+FIGHTER_NAME = 'fighter_name'
 
+
+class LeagueImporter(object):
+
+	# Initialize dictionary to store fighter's event results and list of imported events
 	def __init__(self):
 		self.league_results = {}
 		self.league_events = []
@@ -40,7 +47,7 @@ class Importer(object):
 
 				# ... create a new fighter in league_results if the fighter doesn't yet exist
 				if cur_key not in self.league_results.keys():
-					self.league_results[cur_key] = {"fighter_name": cur_key}
+					self.league_results[cur_key] = {FIGHTER_NAME: cur_key}
 
 				# ... add tournament result to fighter's record
 				self.league_results[cur_key][event_name] = self.get_league_points(cur_place, POINTS_MAP)
@@ -54,9 +61,9 @@ class Importer(object):
 		for fighter in self.league_results:
 			print fighter, self.league_results[fighter]
 
+
 	# Calculates a fighter's total league points accumulated.
 	# max_events specifies how many of the fighter's top results are included in scoring
-
 	def calculate_results(self, max_events=3):
 		for fighter in self.league_results:
 			cur_results = []
@@ -64,29 +71,33 @@ class Importer(object):
 				if event in self.league_results[fighter]:
 					cur_results.append(self.league_results[fighter][event])
 			cur_results.sort(reverse=True)
-			self.league_results[fighter]["league_points"] = sum(cur_results[:max_events])
+			self.league_results[fighter][LEAGUE_POINTS] = sum(cur_results[:max_events])
 			# save the highest unused ranking to be used as a tiebreaker
 			if len(cur_results) > max_events:
-				self.league_results[fighter]["tiebreak"] = cur_results[max_events]
+				self.league_results[fighter][TIE_BREAK] = cur_results[max_events]
 			else:
-				self.league_results[fighter]["tiebreak"] = 0
+				self.league_results[fighter][TIE_BREAK] = 0
 
-	# converts league_results into a ranked list (reverse by points then tiebreak value)
+
+	# converts league_results into a ranked list (reverse by points then tie_break value)
 	def sort_results(self):
 		ordered_results = []
 		for fighter in self.league_results:
 			ordered_results.append(self.league_results[fighter])
-		ordered_results.sort(key=lambda x: (x["league_points"], x["tiebreak"]), reverse=True)
+		ordered_results.sort(key=lambda x: (x[LEAGUE_POINTS], x[TIE_BREAK]), reverse=True)
 		return ordered_results
+
 
 	# writes league results output  
 	def write_league_to_csv(self, output_file):
-		headernames = ['fighter_name'] + self.league_events + ['league_points', 'tiebreak']
+		headernames = [FIGHTER_NAME] + self.league_events + [LEAGUE_POINTS, TIE_BREAK]
 		with open(output_file, 'w') as csvfile:
 			outputwriter = csv.DictWriter(csvfile, fieldnames=headernames, extrasaction='ignore')
 			outputwriter.writeheader()
 			list_to_write = self.sort_results()
 			outputwriter.writerows(list_to_write)
+
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(prog="results_importer")
@@ -94,16 +105,21 @@ if __name__ == '__main__':
 	parser.add_argument('-e', '--event_name_override', action="store_true")
 	parser.add_argument('-w', '--write_output', action="store_true")
 	parser.add_argument('-o', '--output_name', default="default.csv")
+
 	inputs = parser.parse_args()
+
 	event_list = inputs.file_list
 	name_flag = inputs.event_name_override
 
-	league = Importer()
+	league = LeagueImporter()
 
+	# Import counts for verbose messaging
 	count = 0
 	error_count = 0
 
+	# Loop through list of event results
 	for file_name in event_list:
+		# Handles command line flag for overriding file names in export
 		if name_flag:
 			while True:
 				try:
@@ -114,20 +130,38 @@ if __name__ == '__main__':
 			tourney_name = newname
 		else:
 			tourney_name = file_name
-		cur_tourney_file = file_name
+		
+		# Attempt to read from the the current file in the list
 		try:
-			league.read_event_from_csv(tourney_name, cur_tourney_file)
-			print "%s import succeeded" % cur_tourney_file
+			league.read_event_from_csv(tourney_name, file_name)
+			print "%s import successful" % file_name
 			count += 1
+		# CSV import error handling and reporting
 		except:
-			print "%s import failed" % cur_tourney_file
+			print "%s import failed" % file_name
 			error_count += 1
 
+	# Messaging
 	print '%s event file(s) imported' % count
 	if error_count > 0:
 		print '%s event file(s) could not be imported' % error_count
 
-	league.calculate_results(max_events=2)
+	if count > 1:
+		while True:
+			try:
+				max_events = int(raw_input("Enter max # of events for use in scoring: "))
+				if max_events <= 0:
+					print "Entry must be a positive integer"
+					continue
+				else:
+					break
+			except ValueError:
+				print "invalid entry"
+	elif count == 1:
+		max_events = 1
+
+	league.calculate_results(max_events=max_events)
 
 	if inputs.write_output == True:
 		league.write_league_to_csv(inputs.output_name)
+		print 'Output written to file %s' % inputs.output_name
